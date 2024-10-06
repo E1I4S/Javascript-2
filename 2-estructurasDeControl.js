35253f781dc280b30b5bc1861588b1c093b98f8c
E1I4S <eliascardozo013@gmail.com>
2024-10-06 15:43:48 -0300

    Update 2-estructurasDeControl.js

--- 2-estructurasDeControl.js
+++ 2-estructurasDeControl.js
@@ -9,7 +9,7 @@ function verificarParidad(numero) {
     else
     console.log("El numero es impar")
 }
-
+verificarParidad(16)
 // Ejercicio 2: Condicionales anidados
 // Instrucción: Escribe una función que reciba una edad y determine si la persona es:
 // "Menor de edad" (menor a 18), "Adulto" (entre 18 y 65), o "Adulto mayor" (mayor a 65).
@@ -22,7 +22,7 @@ function clasificarEdad(edad) {
 console.log("Adulto mayor");
 }
 }
-
+clasificarEdad(34);
 // Ejercicio 3: Bucles while
 // Instrucción: Crea una función que reciba un número entero positivo y utilice un bucle `while` para imprimir todos los números desde ese número hasta 0.
 function cuentaRegresiva() {
@@ -31,6 +31,7 @@ function cuentaRegresiva() {
             retroceso=retroceso-1;
         }
 }
+cuentaRegresiva(30);
 // Ejercicio 4: Bucles do-while
 // Instrucción: Escribe una función que imprima "Estoy aprendiendo JavaScript" 5 veces utilizando un bucle `do-while`.
 function repetirMensaje() {
@@ -40,7 +41,7 @@ function repetirMensaje() {
         contador=contador+1;
     } while (contador <5);
 }
-
+repetirMensaje();
 // Ejercicio 5: Bucle for
 // Instrucción: Escribe una función que reciba un número entero positivo y utilice un bucle `for` para imprimir todos los números pares entre 0 y ese número.
 function imprimirPares(numero) {
@@ -50,7 +51,7 @@ function imprimirPares(numero) {
         }
      }
 }
-
+imprimirPares(30);
 // Ejercicio 6: Uso de break
 // Instrucción: Escribe una función que recorra los números del 1 al 10, pero detén el bucle cuando el número sea igual a 6.
 function detenerEnSeis() {
@@ -61,7 +62,7 @@ function detenerEnSeis() {
         console.log(recorrer);
     }
 }
-
+ detenerEnSeis();
 // Ejercicio 7: Uso de continue
 // Instrucción: Crea una función que recorra los números del 1 al 10, pero que se salte el número 5 usando `continue`.
 function saltarCinco() {
@@ -72,7 +73,7 @@ function saltarCinco() {
         console.log(salto);
     }
 }
-
+saltarCinco();
 // Ejercicio 8: Switch básico
 // Instrucción: Escribe una función que reciba un número del 1 al 7 y devuelva el día de la semana correspondiente (1 es "Lunes", 2 es "Martes", ..., 7 es "Domingo"). Usa la estructura `switch`.
 function obtenerDiaSemana(dias_de_la_semana) {
@@ -87,7 +88,7 @@ function obtenerDiaSemana(dias_de_la_semana) {
         case 8: console.log("Dia invalido"); break;
     }
 }
-
+obtenerDiaSemana(6);
 // Ejercicio 9: Switch con múltiples casos
 // Instrucción: Escribe una función que reciba un carácter y devuelva si es una vocal. Utiliza un `switch` y agrupa los casos para las vocales (a, e, i, o, u).
 function esVocal(vocal) {
@@ -98,11 +99,12 @@ function esVocal(vocal) {
           case "o":
           case "u":
           console.log("Es vocal");
+          break;
           default:
           console.log("No es vocal");
 }
 }
-
+esVocal(k);
 // Ejercicio 10: Condicionales complejos con operadores lógicos
 // Instrucción: Escribe una función que reciba tres números y determine si todos son positivos, al menos uno es negativo, o todos son negativos. Usa operadores lógicos (`&&`, `||`).
 function evaluarNumeros(A, B, C) {
@@ -116,7 +118,7 @@ function evaluarNumeros(A, B, C) {
         console.log("Hay una mezcla de positivos y negativos");
     }
 }
-
+evaluarNumeros(7,-11 ,-13 )
 // Exportar todas las funciones
 export {
     verificarParidad,
